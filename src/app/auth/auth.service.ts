31ceece04bcbe6d09e3108c177066934cad60525
dmayes403 <davidrmayes@gmail.com>
2018-02-19 14:26:01 -0700

    working on route guard

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,14 +1,25 @@
+import { Subject } from 'rxjs/Subject';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { User } from './user.model';
 import { AuthData } from './auth-data.model';
 
+@Injectable()
 export class AuthService {
+    authChange = new Subject<boolean>();
     private user: User;
+
+    constructor(
+        private router: Router
+    ) {}
 
     registerUser(authData: AuthData) {
         this.user = {
             email: authData.email,
             userId: Math.round(Math.random() * 10000).toString()
         };
+        this.authSuccessfully();
     }
 
     login(authData: AuthData) {
@@ -16,10 +27,14 @@ export class AuthService {
             email: authData.email,
             userId: Math.round(Math.random() * 10000).toString()
         };
+        this.authSuccessfully();
     }
 
     logout() {
         this.user = null;
+        this.authChange.next(false);
+        this.router.navigate(['/login']);
+        
     }
 
     getUser() {
@@ -30,5 +45,10 @@ export class AuthService {
 
     isAuth() {
         return this.user !== null;
+    }
+
+    private authSuccessfully() {
+        this.authChange.next(true);
+        this.router.navigate(['/training']);
     }
 }
