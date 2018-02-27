bb58e5babf68816d8e1e287a2a85a38c40677aef
David Mayes <I21569@xactware.com>
2018-02-27 08:45:34 -0700

    angular fire authentication added

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,53 +1,73 @@
 import { Subject } from 'rxjs/Subject';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 import { User } from './user.model';
 import { AuthData } from './auth-data.model';
+import { TrainingService } from '../training/training.service';
 
 @Injectable()
 export class AuthService {
     authChange = new Subject<boolean>();
-    private user: User;
+    private isAuthenticated = false;
 
     constructor(
-        private router: Router
+        private router: Router,
+        private afAuth: AngularFireAuth,
+        private trainingService: TrainingService
     ) {}
 
+    initAuthListener() {
+        this.afAuth.authState.subscribe(user => {
+            if (user) {
+                this.isAuthenticated = true;
+                this.authChange.next(true);
+                this.router.navigate(['/training']);
+            } else {
+                this.trainingService.cancelSubscriptions();
+                this.authChange.next(false);
+                this.router.navigate(['/login']);
+                this.isAuthenticated = false;
+            }
+        });
+    }
+
     registerUser(authData: AuthData) {
-        this.user = {
-            email: authData.email,
-            userId: Math.round(Math.random() * 10000).toString()
-        };
-        this.authSuccessfully();
+        this.afAuth.auth.createUserWithEmailAndPassword(
+            authData.email,
+            authData.password
+        ).then(result => {
+            console.log(result);
+        })
+        .catch(error => {
+            console.log(error);
+        });
     }
 
     login(authData: AuthData) {
-        this.user = {
-            email: authData.email,
-            userId: Math.round(Math.random() * 10000).toString()
-        };
-        this.authSuccessfully();
+        this.afAuth.auth.signInWithEmailAndPassword(
+            authData.email,
+            authData.password
+        ).then(result => {
+            console.log(result);
+        })
+        .catch(error => {
+            console.log(error);
+        });
     }
 
     logout() {
-        this.user = null;
-        this.authChange.next(false);
-        this.router.navigate(['/login']);
+        this.afAuth.auth.signOut();
     }
 
-    getUser() {
-        return { ...this.user };
+    // getUser() {
+        // return { ...this.user };
         // ^^ this is done to prevent the overriding of the private user variable
         // lecture 55 section 5 @ 6:00
-    }
+    // }
 
     isAuth() {
-        return (this.user && this.user !== null);
-    }
-
-    private authSuccessfully() {
-        this.authChange.next(true);
-        this.router.navigate(['/training']);
+        return this.isAuthenticated;
     }
 }
