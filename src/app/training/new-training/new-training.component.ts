7ff73c70997b30a7bd1b4108012abbf9c76f0ad6
dmayes403 <davidrmayes@gmail.com>
2018-02-27 22:12:17 -0700

    loading state centralized

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -6,6 +6,7 @@ import { map } from 'rxjs/operators';
 import { TrainingService } from '../training.service';
 
 import { Exercise } from '../exercise.model';
+import { UIService } from '../../shared/ui.service';
 
 @Component({
     selector: 'app-new-training',
@@ -14,15 +15,23 @@ import { Exercise } from '../exercise.model';
 })
 export class NewTrainingComponent implements OnInit, OnDestroy {
     exercises: Exercise[];
-    exerciseSubscription: Subscription;
+    private exerciseSubscription: Subscription;
+    private loadingSubscription: Subscription;
+    isLoading = true;
 
     constructor(
         private trainingService: TrainingService,
+        private uiService: UIService
     ) { }
 
     ngOnInit() {
+        this.loadingSubscription = this.uiService.loadingStateChanged.subscribe(isLoading => {
+            this.isLoading = isLoading;
+        });
         this.exerciseSubscription = this.trainingService.exercisesChanged.subscribe(
-            exercises => (this.exercises = exercises)
+            exercises => {
+                this.exercises = exercises
+            }
         );
         this.trainingService.fetchAvailableExercises();
     }
@@ -33,5 +42,6 @@ export class NewTrainingComponent implements OnInit, OnDestroy {
 
     ngOnDestroy() {
         this.exerciseSubscription.unsubscribe();
+        this.loadingSubscription.unsubscribe();
     }
 }
--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -4,6 +4,7 @@ import { AngularFirestore } from 'angularfire2/firestore';
 import { Subscription } from 'rxjs';
 
 import { Exercise } from './exercise.model';
+import { UIService } from '../shared/ui.service';
 
 @Injectable()
 export class TrainingService {
@@ -15,10 +16,12 @@ export class TrainingService {
     private fbSubs: Subscription[] = [];
 
     constructor(
-        private db: AngularFirestore
+        private db: AngularFirestore,
+        private uiService: UIService
     ) {}
 
     fetchAvailableExercises() {
+        this.uiService.loadingStateChanged.next(true);
         this.fbSubs.push(this.db
             .collection('availableExercises')
             .snapshotChanges()
@@ -33,6 +36,7 @@ export class TrainingService {
                 });
             })
             .subscribe((exercises: Exercise[]) => {
+                this.uiService.loadingStateChanged.next(false);
                 this.availableExercises = exercises;
                 this.exercisesChanged.next([...this.availableExercises]);
             }));
